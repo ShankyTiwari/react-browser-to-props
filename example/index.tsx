32a9e9b157b46c30128c281f3c5fe20f57294b00
Shashank <shanshank69@gmail.com>
2020-06-02 11:20:54 +0530

    first commit

---
+++ example/index.tsx
@@ -0,0 +1,20 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import BrowserToProps from '../.';
+
+const App = (props) => {
+  console.log(props)
+  return (
+    <div>
+      Check the console
+    </div>
+  );
+};
+
+const NewComponent = BrowserToProps(App);
+
+ReactDOM.render(
+  <NewComponent />,
+  document.getElementById('root')
+);
